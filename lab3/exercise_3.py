4d76f6f2a1f09a0e3b396ad3458a1643d9111111
Kacper-Sleziak <kacpersleziak00@gmail.com>
2023-11-27 19:47:44 +0100

    lab3

--- lab3/exercise_3.py
+++ lab3/exercise_3.py
@@ -1,13 +1,62 @@
 from sklearn.datasets import make_moons, make_circles, make_gaussian_quantiles
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
-from numpy import std
+from collections import defaultdict
 
 from exercise_1 import CustomClassifier
 from exercise_2 import CustomClassifier as CustomClassifier2
 from icecream import ic
 
 
-def get_results_for_given_model(model, X, y):
+def save_results(data_functions, classifiers):
+    """
+    for every classifier:
+
+    format_of_kf = {
+        'kf2': [],
+        'kf5': []
+    }^
+
+    format_of_results = {
+        'make_circles': format_of_kf,
+        'make_gaussian_quantiles': format_of_kf,
+        'make_moons': format_of_kf
+        }
+    """
+
+    classifiers_results = defaultdict()
+
+    for classifier in classifiers:
+        classifier_results = defaultdict()
+        classifier_name = classifier.__name__
+
+        for data_function in data_functions:
+            function_name = data_function.__name__
+
+            X, y = data_function()
+
+            model = CustomClassifier()
+            score_kf_2, score_kf_5 = get_scores(model, X, y)
+            function_results = {
+                "kf2": score_kf_2,
+                "kf5": score_kf_5,
+            }
+            add_means_and_deviations(function_results)
+
+            classifier_results[function_name] = function_results
+        classifiers_results[classifier_name] = classifier_results
+
+    return classifiers_results
+
+
+def add_means_and_deviations(results):
+    results["kf2_deviation"] = f"{results['kf2'].std():.3f}"
+    results["kf5_deviation"] = f"{results['kf5'].std():.3f}"
+
+    results["kf2_mean"] = f"{results['kf2'].mean():.3f}"
+    results["kf5_mean"] = f"{results['kf5'].mean():.3f}"
+
+
+def get_scores(model, X, y):
     kf_2 = KFold(n_splits=2)
     kf_5 = KFold(n_splits=5)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -19,68 +68,12 @@ def get_results_for_given_model(model, X, y):
     return score_kf_2, score_kf_5
 
 
-def save_results(data_functions):
-    """
-    format_of_kf = {
-        'kf2': [],
-        'kf5': []
-    }
+data_functions = (make_moons, make_circles, make_gaussian_quantiles)
+classifiers = (CustomClassifier, CustomClassifier2)
 
-    format_of_results = {
-        'make_circles': format_of_kf,
-        'make_gaussian_quantiles': format_of_kf,
-        'make_moons': format_of_kf
-        }
-    """
-
-    custom_classifier_results = {}
-    custom_classifier2_results = {}
-
-    for data_function in data_functions:
-        function_name = data_function.__name__
-
-        X, y = data_function()
-
-        model = CustomClassifier()
-        score_kf_2, score_kf_5 = get_results_for_given_model(model, X, y)
-        custom_classifier_results[function_name] = {
-            'kf2': score_kf_2,
-            'kf5': score_kf_5,
-        }
-
-        model = CustomClassifier2(k=5)
-        score_kf_2, score_kf_5 = get_results_for_given_model(model, X, y)
-        custom_classifier2_results[function_name] = {
-            'kf2': score_kf_2,
-            'kf5': score_kf_5,
-        }
-
-    return custom_classifier_results, custom_classifier2_results
+classifiers_results = save_results(data_functions, classifiers)
 
 
-def add_deviations(classifier_results):
-    for function in classifier_results:
-        classifier_results[function]['kf2_deviation'] = f"{classifier_results[function]['kf2'].std():.3f}"
-        classifier_results[function]['kf5_deviation'] = f"{classifier_results[function]['kf5'].std():.3f}"
-
-
-def add_means(classifier_results):
-    for function in classifier_results:
-        classifier_results[function]['kf2_mean'] = f"{classifier_results[function]['kf2'].mean():.3f}"
-        classifier_results[function]['kf5_mean'] = f"{classifier_results[function]['kf5'].mean():.3f}"
-
-
-data_functions = [make_moons, make_circles, make_gaussian_quantiles]
-custom_classifier_results, custom_classifier2_results = save_results(data_functions)
-
-add_deviations(custom_classifier_results)
-add_deviations(custom_classifier2_results)
-
-add_means(custom_classifier_results)
-add_means(custom_classifier2_results)
-
-ic(custom_classifier_results)
+ic(classifiers_results)
 ic("")
-ic(custom_classifier2_results)
-
-
+ic(classifiers_results)
