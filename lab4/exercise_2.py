6ad071d3f31cfea735e38c5dededc3027c6c4054
Kacper-Sleziak <kacpersleziak00@gmail.com>
2023-12-11 16:25:48 +0100

    Fixed exercise2

--- lab4/exercise_2.py
+++ lab4/exercise_2.py
@@ -16,11 +16,10 @@ def save_results(data_functions, classifiers):
         classifier_name = model.__class__.__name__
 
         for data_function in data_functions:
-            X = data_function[0].data
-            y = data_function[0].target
+            data = data_function[0]
             function_name = data_function[1]
 
-            score_kf_2, score_kf_5 = get_scores(model, X, y)
+            score_kf_2, score_kf_5 = get_scores(model, data)
             function_results = {
                 "kf2": score_kf_2,
                 "kf5": score_kf_5,
@@ -32,18 +31,17 @@ def save_results(data_functions, classifiers):
     return classifiers_results
 
 
-def get_scores(model, X, y):
+def get_scores(model, data):
     kf_2 = KFold(n_splits=2)
     kf_5 = KFold(n_splits=5)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = data
 
     # Added scaler
     scaler = StandardScaler()
     scaler.fit(X_train)
 
     X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
 
     model.fit(X_train, y_train)
 
@@ -52,26 +50,53 @@ def get_scores(model, X, y):
     return score_kf_2, score_kf_5
 
 
-data = load_breast_cancer
-data_functions = ([data(), "cancer"],)
+def print_means_and_deviations(classifiers_results):
+    print("")
+    cross_val_scores = {
+        "gaussian_kf2": classifiers_results["GaussianNB"]["cancer"]["kf2"],
+        "gaussian_kf5": classifiers_results["GaussianNB"]["cancer"]["kf5"],
+        "knearest_kf2": classifiers_results["KNeighborsClassifier"]["cancer"]["kf2"],
+        "knearest_kf5": classifiers_results["KNeighborsClassifier"]["cancer"]["kf5"],
+        "decision_tree_kf2": classifiers_results["DecisionTreeClassifier"]["cancer"]["kf2"],
+        "decision_tree_kf5": classifiers_results["DecisionTreeClassifier"]["cancer"]["kf5"],
+    }
+
+    print("---STANDARD DEVIATION---")
+    for key, value in cross_val_scores.items():
+        print(f"{key} - {value.std():.3f}")
+
+    print("")
+
+    print("---MEAN VALUE---")
+    for key, value in cross_val_scores.items():
+        print(f"{key} - {value.mean():.3f}")
+
+data = load_breast_cancer()
+X = data.data
+y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+
+# Standard Scaler for training data only
+data = (X_train, X_test, y_train, y_test)
+data_functions = ([data, "cancer"],)
 classifiers = (KNeighborsClassifier, GaussianNB, DecisionTreeClassifier)
 classifiers_results = save_results(data_functions, classifiers)
 
-cross_val_scores = {
-    "gaussian_kf2": classifiers_results["GaussianNB"]["cancer"]["kf2"],
-    "gaussian_kf5": classifiers_results["GaussianNB"]["cancer"]["kf5"],
-    "knearest_kf2": classifiers_results["KNeighborsClassifier"]["cancer"]["kf2"],
-    "knearest_kf5": classifiers_results["KNeighborsClassifier"]["cancer"]["kf5"],
-    "decision_tree_kf2": classifiers_results["DecisionTreeClassifier"]["cancer"]["kf2"],
-    "decision_tree_kf5": classifiers_results["DecisionTreeClassifier"]["cancer"]["kf5"],
-}
+print("TRAINING DATA SCALED")
+print_means_and_deviations(classifiers_results)
 
-print("---STANDARD DEVIATION---")
-for key, value in cross_val_scores.items():
-    print(f"{key} - {value.std():.3f}")
+print("\n\n")
 
-print("")
+# Standard Scaler for training and test data
+X_test = scaler.transform(X_test)
+data = (X_train, X_test, y_train, y_test)
+data_functions = ([data, "cancer"],)
+classifiers = (KNeighborsClassifier, GaussianNB, DecisionTreeClassifier)
 
-print("---MEAN VALUE---")
-for key, value in cross_val_scores.items():
-    print(f"{key} - {value.mean():.3f}")
+classifiers_results = save_results(data_functions, classifiers)
+print("TRAINING AND TEST DATA SCALED")
+print_means_and_deviations(classifiers_results)
+
