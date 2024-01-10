935579601808f2a5750f74a0f7e53ee87fd3bd37
Kacper-Sleziak <kacpersleziak00@gmail.com>
2024-01-10 07:00:05 +0100

    lab6 exercise 3

--- lab_6/exercise_3.py
+++ lab_6/exercise_3.py
@@ -10,28 +10,36 @@ from sklearn.utils import check_X_y
 
 
 class EnsembleClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, n_classifiers, soft, base_classifier=GaussianNB()):
+    def __init__(self, n_classifiers, soft, weighted, base_classifier=GaussianNB()):
+        self.weighted = weighted
         self.soft = soft
         self.n_classifiers = n_classifiers
         self.base_classifier = base_classifier
         self.classifiers = []
+        self.weighted_pred = []
 
     def fit(self, X, y):
         X, y = check_X_y(X, y)
 
         training_size = ceil(sqrt(len(X)))
         for _ in range(self.n_classifiers):
-            indices = np.random.choice(len(X), training_size, replace=True)
-            X_bootstrap, y_bootstrap = X[indices], y[indices]
+            pool = np.random.choice(len(X), training_size, replace=True)
+            X_bootstrap, y_bootstrap = X[pool], y[pool]
 
             classifier = self.base_classifier.fit(X_bootstrap, y_bootstrap)
             self.classifiers.append(classifier)
 
+            prediction = clf.predict(X)
+            self.weighted_pred.append(accuracy_score(y, prediction))
+
     def predict(self, X):
+        if not self.soft and self.weighted:
+            raise Exception("Weighted flag only works with soft voting!")
+
         if self.soft:
-            final_predictions = self.vote_soft(X)
-        else:
             final_predictions = self.vote_hard(X)
+        else:
+            final_predictions = self.vote_soft(X)
         return final_predictions
 
     def vote_hard(self, X):
@@ -45,15 +53,21 @@ class EnsembleClassifier(BaseEstimator, ClassifierMixin):
     def vote_soft(self, X):
         classifiers_results = np.zeros(shape=(X.shape[0], len(classifiers)))
 
-        for classifier in self.classifiers:
-            classifiers_results += classifier.predict_proba(X)
+        if self.weighted:
+            for i, classifier in enumerate(self.classifiers):
+                classifiers_results += (
+                    classifier.predict_proba(X) * self.weighted_pred[i]
+                )
+        else:
+            for classifier in self.classifiers:
+                classifiers_results += classifier.predict_proba(X)
         final_predictions = np.argmax(classifiers_results, axis=1)
 
         return final_predictions
 
 
 rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5)
-classifiers = [EnsembleClassifier(5, True), EnsembleClassifier(5, False)]
+classifiers = [EnsembleClassifier(5, True, True), EnsembleClassifier(5, True, False)]
 results = np.empty(shape=(10, len(classifiers)))
 
 X, y = make_classification()
@@ -68,5 +82,5 @@ for i, (train_index, test_index) in enumerate(rskf.split(X, y)):
         accuracy = accuracy_score(y[test_index], predict_y)
         results[i, classifier_id] = accuracy
 
-for i, classifier in enumerate(["soft voting", "hard voting"]):
+for i, classifier in enumerate(["Weighted soft", "Not weighted soft"]):
     print(f"Result of {classifier}: {(round(np.mean(results[i]), 3))}")
